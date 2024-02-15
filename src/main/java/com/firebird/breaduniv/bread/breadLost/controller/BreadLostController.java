d72ab33df0bf30308117d24459e73b6180e9969d
Thea Kim <kimmjnha@gmail.com>
2024-02-15 11:04:03 +0900

    24/02/15/11/04

---
+++ src/main/java/com/firebird/breaduniv/bread/breadLost/controller/BreadLostController.java
@@ -0,0 +1,28 @@
+package com.firebird.breaduniv.bread.breadLost.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BreadLostController {
+
+    @RequestMapping("/breadLostAdmin")
+    public String breadLostAdmin() {
+        return "/bread/breadLost/breadLostAdmin";
+    }
+
+    @RequestMapping("/breadLostModify")
+    public String breadLostModify() {
+        return "/bread/breadLost/breadLostModify";
+    }
+
+    @RequestMapping("/breadLostPage")
+    public String breadLostPage() {
+        return "/bread/breadLost/breadLostPage";
+    }
+
+    @RequestMapping("/breadLostPageAdmin")
+    public String breadLostPageAdmin() {
+        return "/bread/breadLost/breadLostPageAdmin";
+    }
+}
