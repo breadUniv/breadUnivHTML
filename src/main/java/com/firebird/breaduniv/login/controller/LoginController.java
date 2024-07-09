719c63c9b784923983398c5a9fb9f07744bdf7c5
Thea Kim <kimmjnha@gmail.com>
2024-07-09 20:51:25 +0900

    editDB

--- src/main/java/com/firebird/breaduniv/login/controller/LoginController.java
+++ src/main/java/com/firebird/breaduniv/login/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping("/enrollment")
+    // 여기에서
     public String enrollment() {
         return "/login/enrollment";
     }
